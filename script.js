9d59305dfd86346c7f98ee5bfa30f6225baed194
sdinesh9676 <167186574+sdinesh9676@users.noreply.github.com>
2024-12-05 11:57:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Menu Toggle
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
